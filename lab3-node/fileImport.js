6f54ed59924124a77a2bed3becc93271938f9e7b
Mayaa <mayastuds@gmail.com>
2025-11-11 18:07:40 +0100

    Add Lab3 Node.js exercises

---
+++ lab3-node/fileImport.js
@@ -0,0 +1,4 @@
+const mean = require('./notation');
+
+const notes = [10, 12, 14, 16, 18];
+console.log("Average:", mean(notes));
---
+++ lab3-node/notation.js
@@ -0,0 +1,10 @@
+function mean(scores) {
+  let sum = 0;
+  for (let i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  }
+  return sum / scores.length;
+}
+
+
+module.exports = mean;
